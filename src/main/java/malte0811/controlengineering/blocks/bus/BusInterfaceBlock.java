d29435c4331fb7daf7118b2c43317279553ccc4f
malte0811 <malte0811@web.de>
2022-03-19 15:52:43 +0100

    Drop bus interface when supporting block is removed

--- src/main/java/malte0811/controlengineering/blocks/bus/BusInterfaceBlock.java
+++ src/main/java/malte0811/controlengineering/blocks/bus/BusInterfaceBlock.java
@@ -5,7 +5,10 @@ import malte0811.controlengineering.blocks.CEBlock;
 import malte0811.controlengineering.blocks.placement.BlockPropertyPlacement;
 import malte0811.controlengineering.blocks.shapes.DirectionalShapeProvider;
 import malte0811.controlengineering.blocks.shapes.FromBlockFunction;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -39,5 +42,17 @@ public class BusInterfaceBlock extends CEBlock<Direction> {
     protected void createBlockStateDefinition(@Nonnull StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
         builder.add(FACING);
+    }
+
+    @Override
+    public void neighborChanged(
+            @Nonnull BlockState state, @Nonnull Level world, @Nonnull BlockPos pos,
+            @Nonnull Block block, @Nonnull BlockPos fromPos, boolean isMoving
+    ) {
+        super.neighborChanged(state, world, pos, block, fromPos, isMoving);
+        if (world.isEmptyBlock(pos.relative(state.getValue(FACING)))) {
+            popResource(world, pos, new ItemStack(this));
+            world.removeBlock(pos, false);
+        }
     }
 }
